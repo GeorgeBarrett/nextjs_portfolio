20c3747c66d651d6035c9b30ed29e47fedddc0e3
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-06 12:18:10 +0000

    burger menu animation built using state and javascript logic

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+
 
 const Navbar = () => {
+
+    const [navigation, setNavigation] = useState(false)
 
     const links = [
         {
@@ -30,7 +34,7 @@ const Navbar = () => {
         <div className=" w-full h-20 z-10 fixed bg-white text-black duration-300 ease-in">
             <div className="flex justify-between items-center w-full h-full max-w-screen-xl mx-auto p-4">
                 <Link href="/#home" >
-                    <h1 className="text-3xl lg:text-4xl font-bold uppercase underline-offset-2 trackingwider cursor-pointer">George</h1>
+                    <h1 className="text-3xl lg:text-4xl font-bold uppercase underline underline-offset-2 trackingwider cursor-pointer">George</h1>
                 </Link >
                 
                 <div>
@@ -43,11 +47,42 @@ const Navbar = () => {
                                 </Link>
                             ))}
                         
-
                     </ul>
-                </div>
 
+                    {
+                        !navigation && (
+                        <div className="md:hidden cursor-pointer" onClick={() => setNavigation(true)}>
+                            <FaBars size={30} />
+                        </div>
+                        )
+                    }
+
+                </div>
             </div>
+
+            <div className={
+                navigation 
+                    ? "md:hidden fixed left-0 top-0 w-full h-full bg-black/20 backdrop-blur"
+                    : ""}
+                >    
+                    <div className={
+                        navigation
+                        ? "left-0 fixed top-0 w-4/5 h-full bg-gradient-to-r from-black to-gray-600 text-white p-10 ease-in duration-500"
+                        : "fixed top-0 left-[-100%] p-10 h-full ease-in duration-500"}>
+                
+                    <div>
+                        <div className="flex w-full items-center justify-between">
+                            <Link href="/#home">
+                                <h2 className="text-3xl font-bold uppercase underline underline-offset-2 tracking-wider cursor-pointer">George</h2>
+                            </Link>
+                            <div onClick={() => setNavigation(false)} className="p-3 cursor-pointer">
+                                <FaTimes size={30} />
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>                    
         </div>
     );
 };
