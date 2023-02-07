8204ca39a598566e96d6a171a12c0b7371fac304
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-02-07 15:57:11 +0000

    navbar toggles off once link has been pushed in mobile version. navbar opacity changes when user scrolls down past a certain point. smooth scroll is working

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,37 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+
 
 
 const Navbar = () => {
 
     const [navigation, setNavigation] = useState(false)
 
+    const [pageScroll, setPageScroll] = useState(false)
+
+    useEffect(() => {
+        const sub = addEventListener('scroll', () => setPageScroll(window.scrollY >= 130))
+
+        return sub;
+
+    }, [])
+
     const links = [
         {
             id: 1,
-            link: 'home'
+            link: "home"
         },
         {
             id: 2,
-            link: 'portfolio'
+            link: "portfolio"
         },
         {
             id: 3,
-            link: 'experience'
+            link: "experience"
         },
         {
             id: 4,
-            link: 'me'
+            link: "me"
         },
         {
             id: 5,
-            link: 'contact'
+            link: "contact"
         },
     ]
 
     return (
-        <div className=" w-full h-20 z-10 fixed bg-white text-black duration-300 ease-in">
+        <div className={`w-full h-20 z-10 fixed bg-white text-black duration-300 ease-in ${pageScroll && "bg-[#fefefea7]"}`}>
             <div className="flex justify-between items-center w-full h-full max-w-screen-xl mx-auto p-4">
                 <Link href="/#home" >
                     <h1 className="text-3xl lg:text-4xl font-bold uppercase underline underline-offset-2 trackingwider cursor-pointer">George</h1>
@@ -73,7 +83,7 @@ const Navbar = () => {
                     <div>
                         <div className="flex w-full items-center justify-between">
                             <Link href="/#home">
-                                <h2 className="text-3xl font-bold uppercase underline underline-offset-2 tracking-wider cursor-pointer">George</h2>
+                                <h2 onClick={() => setNavigation(false)} className="text-3xl font-bold uppercase underline underline-offset-2 tracking-wider cursor-pointer">George</h2>
                             </Link>
                             <div onClick={() => setNavigation(false)} className="p-3 cursor-pointer">
                                 <FaTimes size={30} />
@@ -86,7 +96,7 @@ const Navbar = () => {
 
                             {links.map(({id, link}) => (
                                 <Link key={id} href={`/#${link}`}>
-                                    <li className="py-4 text-2xl tracking-wider cursor-pointer">{link}</li>
+                                    <li onClick={() => setNavigation(false)} className="py-4 text-2xl tracking-wider cursor-pointer">{link}</li>
                                 </Link>
                             ))}
 
