1d7581fd0efe129f85545353a407c639f19a0b99
GeorgeBarrett <georgeaustinbarrett@gmail.com>
2023-03-09 14:58:50 +0000

    components imported and routing organised

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import Experience from "@/components/Experience";
+import Portfolio from "@/components/Portfolio";
+import HomePage from "@/components/HomePage";
+import Me from "@/components/Me";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hola</h1>
+      <HomePage />
+      <Portfolio />
+      <Experience />
+      <Me />
+      <Contact />
     </div>
-  )
+  );
 }
